6e9e43b66703b12fea85cbd885b326bdb247185f
Aaditya Kumar <aadityakumar5892@gmail.com>
2024-12-25 21:38:38 +0530

    Time: 1 ms (100%), Space: 42.7 MB (20.52%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,25 +1,28 @@
-class Solution 
-{
-    public int myAtoi(String s) 
-    {
+class Solution {
+    public int myAtoi(String s) {
         s = s.trim();
-        if (s.isEmpty()) return 0;
-
-        int sign = 1, n = 0, i = 0;
-        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-            sign = (s.charAt(i) == '-') ? -1 : 1;
-            i++;
-        }
-
-        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-            int digit = s.charAt(i) - '0';
-            if (n > (Integer.MAX_VALUE - digit) / 10) {
-                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        int i = 0;
+        if(s.isEmpty()) return 0;
+        int digit = 0;
+        int result = 0;
+        int sign = 1;
+            if(s.charAt(i) == '-'){
+                i++;
+                sign = -1;
             }
-            n = n * 10 + digit;
-            i++;
+            else if(s.charAt(i) == '+'){
+                i++;
+                sign = 1;
+            }
+            while(i<s.length() &&(s.charAt(i) <= 57 && s.charAt(i) >= 48)){
+                digit = s.charAt(i) -'0';
+                
+                if((result > Integer.MAX_VALUE/10) || (result == Integer.MAX_VALUE/10 && digit >7))
+                return sign == 1 ? Integer.MAX_VALUE :Integer.MIN_VALUE;
+                result = result * 10 + digit;
+                i++;
+            }
+            return result*sign;
         }
-        
-        return n * sign;
+
     }
-}
