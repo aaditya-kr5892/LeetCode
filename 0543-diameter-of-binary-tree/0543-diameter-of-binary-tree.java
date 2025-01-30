8e7b8894d191a2dc51d51b646a3e54c10a5d091b
Aaditya Kumar <aadityakumar5892@gmail.com>
2025-01-30 10:03:20 +0530

    Time: 0 ms (100%), Space: 44.7 MB (62.2%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,20 +13,27 @@
  *     }
  * }
  */
+ class data{
+    int value;
+    data(int value){
+        this.value = value;
+    }
+ }
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        int arr[] = new int[1];
-        int max = f(root,arr);
-        return arr[0];
+        data d = new data(0);
+        //int arr[] = new int[1];
+        int max = f(root,d);
+        return d.value;
     }
-    int f(TreeNode root,int[] arr){
+    int f(TreeNode root,data d){
         if(root == null){
             return 0;
         }
-        int l = f(root.left,arr);
-        int r = f(root.right,arr);
-        if(l+r > arr[0]){
-            arr[0] = l+r;
+        int l = f(root.left,d);
+        int r = f(root.right,d);
+        if(l+r > d.value){
+            d.value = l+r;
         }
         return 1 + Math.max(l,r);
     }
