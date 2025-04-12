3eb93569417225fefbc8b0eabc3512528311a0e9
Aaditya Kumar <aadityakumar5892@gmail.com>
2025-04-12 12:38:01 +0530

    Time: 5 ms (79.31%), Space: 45.9 MB (62.23%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -3,8 +3,7 @@ class Solution {
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         int topo[] = new int[numCourses];
         int indegree[] = new int[numCourses];
-        int visited[] = new int[numCourses];
-        int path[] = new int[numCourses];
+        
         for(int i = 0 ; i < numCourses ; i++){
             list.add(new ArrayList<>());
         }
@@ -20,12 +19,7 @@ class Solution {
         }
         if(que.isEmpty())
         return new int[0];
-        for(int i = 0 ; i < numCourses ; i++){
-            if(visited[i] == 0){
-                if(dfs(list,visited,path,i))
-                return new int[0];
-            }
-        }
+        
         int k = 0 ;
         while(!que.isEmpty()){
             int t = que.poll();
@@ -37,24 +31,9 @@ class Solution {
                 }
             }
         }
+        if(k == indegree.length)
         return topo;
-    }
-    boolean dfs(ArrayList<ArrayList<Integer>> adj,int visited[],int path[],int node){
-        if(visited[node] == 0){
-            visited[node] = 1;
-            path[node] = 1;
-            for(int i = 0 ; i < adj.get(node).size() ; i++){
-                if(dfs(adj,visited,path,adj.get(node).get(i)))
-                return true;
-            }
-            path[node] = 0;
-        }
-        else{
-            if(path[node] == 1){
-                return true;
-            }
-            
-        }
-        return false;
+        else
+        return new int[0];
     }
 }
