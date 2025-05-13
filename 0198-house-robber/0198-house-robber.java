8f013f80ecc3c30f8b924377a14385699586e825
Aaditya Kumar <aadityakumar5892@gmail.com>
2025-05-13 06:06:13 +0530

    Time: 0 ms (100%), Space: 40.9 MB (76.37%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp,-1);
+        return f(nums.length-1,nums,dp);
+    }
+    int f(int ind,int [] nums,int [] dp){
+        if(ind == 0) return nums[ind];
+        if(ind < 0) return 0;
+        if(dp[ind] != -1)
+        return dp[ind];
+        int pick = nums[ind] + f(ind-2,nums,dp);
+        int not = f(ind-1,nums,dp);
+        return dp[ind] = Math.max(pick,not);
+    }
+}
