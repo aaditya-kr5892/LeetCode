e3482e5c6551707926abc403e1c2d27276ab5ece
Aaditya Kumar <aadityakumar5892@gmail.com>
2025-05-15 21:18:33 +0530

    Time: 9 ms (20.4%), Space: 45.6 MB (29.75%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,32 +1,30 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int dp[][] = new int[matrix.length][matrix[0].length];
-        for(int i = 0 ; i < matrix.length ; i++){
-            for(int j = 0 ; j < matrix[0].length ; j++){
-                dp[i][j] = Integer.MIN_VALUE;
+        
+        int min = Integer.MAX_VALUE;
+        for(int i =0 ; i < matrix[0].length ; i++){
+            dp[0][i] = matrix[0][i];
+        }
+        for(int i = 1 ; i < matrix.length ; i++){
+            for(int j = 0 ; j < matrix[i].length ; j++){
+                int left = Integer.MAX_VALUE, top = Integer.MAX_VALUE, right = Integer.MAX_VALUE;
+                if(i > 0 && j > 0){
+                    left = matrix[i][j] + dp[i-1][j-1];
+                }
+                if(i > 0 && j < matrix[i].length-1){
+                    right = matrix[i][j] + dp[i-1][j+1];
+                }
+                if(i > 0){
+                    top = matrix[i][j] + dp[i-1][j];
+                }
+                dp[i][j] = Math.min(left,Math.min(right,top));
             }
         }
-        int min = Integer.MAX_VALUE;
         for(int i = 0 ; i < matrix[matrix.length-1].length ; i++){
-            min = Math.min(min,f(matrix.length-1,i,matrix,dp));
+            min = Math.min(min,dp[matrix.length-1][i]);
         }
         return min;
     }
-    int f(int i, int j, int matrix[][] , int dp[][]){
-        if(i == 0){
-            return matrix[i][j];
-        }
-        if(dp[i][j] != Integer.MIN_VALUE) return dp[i][j];
-        int left = Integer.MAX_VALUE, top = Integer.MAX_VALUE, right = Integer.MAX_VALUE;
-        if(i > 0 && j > 0){
-            left = matrix[i][j] + f(i-1,j-1,matrix,dp);
-        }
-        if(i > 0 && j < matrix[i].length-1){
-            right = matrix[i][j] + f(i-1,j+1,matrix,dp);
-        }
-        if(i > 0){
-            top = matrix[i][j] + f(i-1,j,matrix,dp);
-        }
-        return dp[i][j] = Math.min(left,Math.min(right,top));
-    }
+    
 }
