57c81f3cb965252cb65c52c372d153cbfada9ab2
Aaditya Kumar <aadityakumar5892@gmail.com>
2025-07-06 01:28:08 +0530

    Time: 5 ms (60.02%), Space: 45.1 MB (45.97%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,12 +1,22 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()][word2.length()];
-        for(int i = 0 ; i < dp.length ; i++){
-            for(int j = 0 ; j< dp[0].length ; j++){
-                dp[i][j] = -1;
+        int dp[][] = new int[word1.length()+1][word2.length()+1];
+        for(int i = 1 ; i < dp.length ; i++){
+            dp[i][0] = i;
+        }
+        for(int j = 1 ; j < dp[0].length ; j++){
+            dp[0][j] = j;
+        }
+        for(int i = 1 ; i < dp.length ; i++){
+            for(int j = 1 ; j < dp[0].length ; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else
+                dp[i][j] = 1+Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
             }
         }
-        return f(word1.length()-1,word2.length()-1,word1,word2,dp);
+        return dp[word1.length()][word2.length()];
     }
     int f(int i, int j, String s1, String s2,int dp[][]){
         if (i < 0) return j + 1;
