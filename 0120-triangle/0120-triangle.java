c03f440174b1e396845cc76b3b6b0b1d69a55ec2
Aaditya Kumar <aadityakumar5892@gmail.com>
2025-09-25 23:45:11 +0530

    Time: 16 ms (6%), Space: 45.5 MB (42.22%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -10,22 +10,22 @@ class Solution {
             }
         }
         int min = Integer.MAX_VALUE;
+        dp.get(0).set(0,triangle.get(0).get(0));
+        for(int i = 1 ; i < dp.size() ; i++){
+            for(int j = 0 ; j < dp.get(i).size() ; j++){
+                int down = Integer.MAX_VALUE;
+                int dl = Integer.MAX_VALUE;
+                if(i > 0 && j > 0)
+                dl = triangle.get(i).get(j) + dp.get(i-1).get(j-1);
+                if(i > 0 && j < triangle.get(i).size()-1)
+                down = triangle.get(i).get(j) + dp.get(i-1).get(j);
+                dp.get(i).set(j,Math.min(down,dl));
+            }
+        }
         for(int i = 0 ; i < triangle.get(triangle.size()-1).size() ; i++){
-            min = Math.min(min,f(triangle.size()-1, i, triangle, dp));
+            min = Math.min(min,dp.get(triangle.size()-1).get(i));
         }
         return min;
     }
-    int f(int i, int j, List<List<Integer>> list, List<List<Integer>> dp){
-        if(i == 0 && j == 0 ){
-            return list.get(0).get(0);
-        }
-        if(dp.get(i).get(j) != -1) return dp.get(i).get(j);
-        int down = Integer.MAX_VALUE , dl = Integer.MAX_VALUE;
-        if(i > 0 && j > 0)
-        dl = list.get(i).get(j) + f(i-1,j-1,list,dp);
-        if(i > 0 && j < list.get(i).size()-1)
-        down = list.get(i).get(j) + f(i-1,j,list,dp);
-        dp.get(i).set(j,Math.min(down,dl));
-        return dp.get(i).get(j);
-    }
+    
 }
