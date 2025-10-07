12eaea6478b103f548f7e6d8b3162dc31a65f183
Aaditya Kumar <aadityakumar5892@gmail.com>
2025-10-07 17:51:09 +0530

    Time: 18 ms (13.8%), Space: 48.1 MB (17.49%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,44 +1,45 @@
-class custom{
+class data{
     int i;
     int j;
-    int level;
-
-    custom(int i,int j,int level){
+    int depth;
+    data(int i, int j, int depth){
         this.i = i;
         this.j = j;
-        this.level = level;
+        this.depth = depth;
     }
 }
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int v[][] = new int[n][m];
-        Queue<custom> que = new LinkedList<>();
-        int visited[][] = new int[mat.length][mat[0].length];
+        Queue<data> que = new LinkedList<>();
+        int v[][] = new int[mat.length][mat[0].length];
+        int visited[][] = new int [mat.length][mat[0].length];
         for(int i = 0 ; i < mat.length ; i++){
             for(int j = 0 ; j < mat[0].length ; j++){
-                visited[i][j] = mat[i][j];
                 if(mat[i][j] == 0){
-                    v[i][j] = 1;
-                    que.add(new custom(i,j,0));
+                    que.add(new data(i, j, 0));
                 }
+                v[i][j] = Integer.MAX_VALUE;
             }
         }
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,1,0,-1};
+
         while(!que.isEmpty()){
-            custom t = que.poll();
+            data d = que.poll();
+            if(mat[d.i][d.j] == 0)
+            v[d.i][d.j] = 0;
+            int drow[] = {0, 1, 0, -1};
+            int dcol[] = {1, 0, -1, 0};
             for(int i = 0 ; i < 4 ; i++){
-                int nrow = t.i + drow[i];
-                int ncol = t.j + dcol[i];
-                if(nrow < n && ncol < m && ncol >= 0 && nrow >= 0 && visited[nrow][ncol] == 1 && v[nrow][ncol] == 0){
-                    visited[nrow][ncol] = t.level+1;
-                    que.add(new custom(nrow,ncol,visited[nrow][ncol]));
-                    v[nrow][ncol] = 1;
+                int nrow = d.i + drow[i];
+                int ncol = d.j + dcol[i];
+                if(nrow >= 0 && ncol >= 0 && nrow < mat.length && ncol < mat[0].length){
+                    if(visited[nrow][ncol] == 0 && mat[nrow][ncol] == 1){
+                        que.add(new data(nrow, ncol, d.depth+1));
+                        v[nrow][ncol] = Math.min(d.depth+1, v[nrow][ncol]);
+                        visited[nrow][ncol] = 1;
+                    }
                 }
             }
         }
-        return visited;
+        return v;
     }
 }
