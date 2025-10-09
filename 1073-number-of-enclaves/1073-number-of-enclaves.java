99223530bc12ce58426ff76dc3eea0840fffa9cc
Aaditya Kumar <aadityakumar5892@gmail.com>
2025-10-09 23:22:18 +0530

    Time: 11 ms (16.84%), Space: 53.2 MB (90.38%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -1,46 +1,62 @@
+class data{
+    int i;
+    int j;
+    data(int i, int j){
+        this.i = i;
+        this.j = j;
+    }
+}
 class Solution {
     public int numEnclaves(int[][] grid) {
         int visited[][] = new int[grid.length][grid[0].length];
-        for(int i = 0 ; i < grid.length ; i++){
-            if(visited[i][0] == 0 && grid[i][0] == 1){
-                visited[i][0] = 1;
-                dfs(i,0,visited,grid);
-            }
-            if(visited[i][grid[0].length-1] == 0 && grid[i][grid[0].length-1] == 1){
-                visited[i][grid[0].length-1] = 1;
-                dfs(i,grid[0].length-1 , visited,grid);
-            }
-        }
-        for(int j = 0 ; j < grid[0].length ; j++){
-            if(visited[0][j] == 0 && grid[0][j] == 1){
-                visited[0][j] = 1;
-                dfs(0,j,visited,grid);
-            }
-            if(visited[grid.length-1][j] == 0 && grid[grid.length-1][j] == 1){
-                visited[grid.length-1][j] = 1;
-                dfs(grid.length-1,j,visited,grid);
-            }
-        }
+        Queue<data> que = new LinkedList<>();
+
         int count = 0 ;
+
+        for(int i = 0 ; i < grid.length ; i++){
+            if(grid[i][0] == 1){
+                que.add(new data(i, 0));
+            }
+            if(grid[i][grid[0].length-1] == 1){
+                que.add(new data(i, grid[0].length-1));
+            }
+        }
+
+        for(int i = 0 ; i < grid[0].length ; i++){
+            if(grid[0][i] == 1){
+                que.add(new data(0, i));
+            }
+            if(grid[grid.length-1][i] == 1){
+                que.add(new data(grid.length-1, i));
+            }
+        }
+
+        while(!que.isEmpty()){
+            data d = que.poll();
+            visited[d.i][d.j] = 1;
+            int drow[] = {0, 1, 0, -1};
+            int dcol[] = {1, 0, -1, 0};
+            for(int i = 0 ; i < 4 ; i++){
+                int nrow = d.i + drow[i];
+                int ncol = d.j + dcol[i];
+                if(nrow >= 0 && ncol >= 0 && nrow < grid.length && ncol < grid[0].length){
+                    if(grid[nrow][ncol] == 1 && visited[nrow][ncol] == 0){
+                        visited[nrow][ncol] = 1;
+                        que.add(new data(nrow, ncol));
+                    }
+                }
+            }
+        }
+
         for(int i = 0 ; i < grid.length ; i++){
             for(int j = 0 ; j < grid[0].length ; j++){
-                if(visited[i][j] != 1 && grid[i][j] == 1){
+                if(visited[i][j] == 0 && grid[i][j] == 1){
                     count++;
                 }
             }
         }
+
         return count;
-    }
-    void dfs(int i , int j , int[][]visited,int[][] board){
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,1,0,-1};
-        for(int i1 = 0 ; i1 < 4 ; i1++){
-            int nrow = i + drow[i1];
-            int ncol = j + dcol[i1];
-            if(nrow >= 0 && ncol <= board[0].length-1 && ncol >= 0 && nrow <= board.length-1 && visited[nrow][ncol] != 1 && board[nrow][ncol] == 1){
-                visited[nrow][ncol] = 1;
-                dfs(nrow,ncol,visited,board);
-            }
-        }
+
     }
 }
