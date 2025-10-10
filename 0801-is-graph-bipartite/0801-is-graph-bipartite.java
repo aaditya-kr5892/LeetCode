a6bee5862e4a527fd6195c70de344003653b1fbf
Aaditya Kumar <aadityakumar5892@gmail.com>
2025-10-11 02:25:07 +0530

    Time: 0 ms (100%), Space: 45 MB (82.01%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,46 +1,30 @@
-class custom{
-    int node;
-    int parent;
-    int depth;
-    custom(int node,int parent,int depth){
-        this.node = node;
-        this.parent = parent;
-        this.depth = depth;
-    }
-}
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int visited[] = new int[graph.length];
-        for(int i = 0 ; i < visited.length ; i++){
-            if(visited[i] == 0){
-                if(bfs(graph,i,visited))
-                return false;
+        int n = graph.length;
+        int[] visited = new int[n];
+        int[] depth = new int[n];  
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                if (!dfs(i, -1, 0, visited, depth, graph)) return false;
             }
-            
         }
         return true;
     }
-    boolean bfs(int graph[][] ,int node,int visited[]){
-        Queue<custom> que = new LinkedList<>();
+
+    boolean dfs(int node, int parent, int level, int[] visited, int[] depth, int[][] graph) {
         visited[node] = 1;
-        int dist[] = new int[visited.length];
-        dist[node] = 0;
-        que.add(new custom(node,-1,0));
-        //int sh = Integer.MAX_VALUE;
-        while(!que.isEmpty()){
-            custom t = que.poll();
-            for(int j = 0 ; j < graph[t.node].length ; j++){
-                if(visited[graph[t.node][j]] == 0){
-                    visited[graph[t.node][j]] = 1;
-                    dist[graph[t.node][j]] = t.depth+1;
-                    que.add(new custom(graph[t.node][j],t.node,t.depth+1));
-                }
-                else if(graph[t.node][j] != t.parent){
-                    int cycleLength = dist[t.node] + dist[graph[t.node][j]] + 1;
-                    if (cycleLength % 2 != 0) return true;
-                }
+        depth[node] = level;
+
+        for (int neighbor : graph[node]) {
+            if (visited[neighbor] == 0) {
+                if (!dfs(neighbor, node, level + 1, visited, depth, graph)) return false;
+            } 
+            else if (neighbor != parent) {
+                int cycleLength = Math.abs(depth[node] - depth[neighbor]) + 1;
+                if (cycleLength % 2 == 1) return false; 
             }
         }
-        return false;
+        return true;
     }
 }
