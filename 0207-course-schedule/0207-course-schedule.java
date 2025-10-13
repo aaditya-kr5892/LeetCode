eff13b68b3f6107baa2f816ffafab419161f3e86
Aaditya Kumar <aadityakumar5892@gmail.com>
2025-10-13 07:31:22 +0530

    Time: 8 ms (26.63%), Space: 45.4 MB (61.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,35 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        int visited[] = new int[numCourses];
-        int path[] = new int[numCourses];
-        
+    public boolean canFinish(int numCourses, int[][] pre) {
+        int indegree[] = new int[numCourses];
+        List<List<Integer>> list = new ArrayList<>();
         for(int i = 0 ; i < numCourses ; i++){
             list.add(new ArrayList<>());
         }
-        for(int i = 0; i < prerequisites.length ; i++){
-            list.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        Queue<Integer> que = new LinkedList<>();
+        for(int i = 0 ; i< pre.length ; i++){
+            indegree[pre[i][0]]++;
+            list.get(pre[i][1]).add(pre[i][0]);
         }
-        for(int i = 0 ; i < numCourses ; i++){
-            if(visited[i] == 0){
-                if(dfs(list,visited,path,i))
-                return false;
+        for(int i = 0; i < indegree.length ; i++){
+            if(indegree[i] ==0){
+                que.add(i);
             }
         }
-        return true;
-    }
-    boolean dfs(ArrayList<ArrayList<Integer>> adj,int visited[],int path[],int node){
-        if(visited[node] == 0){
-            visited[node] = 1;
-            path[node] = 1;
-            for(int i = 0 ; i < adj.get(node).size() ; i++){
-                if(dfs(adj,visited,path,adj.get(node).get(i)))
-                return true;
-            }
-            path[node] = 0;
+        if(que.size() == 0){
+            return false;
         }
-        else{
-            if(path[node] == 1){
-                return true;
+        int count = 0;
+        while(!que.isEmpty()){
+            int node = que.poll() ;
+            count++;
+            for(int i = 0 ; i < list.get(node).size() ; i++){
+                indegree[list.get(node).get(i)]--;
+                if(indegree[list.get(node).get(i)] == 0){
+                    que.add(list.get(node).get(i));
+                }
             }
-            
         }
-        return false;
+        if(count != numCourses) return false;
+        else return true;
     }
 }
