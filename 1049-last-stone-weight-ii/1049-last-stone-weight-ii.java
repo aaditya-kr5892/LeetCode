a018d2a8e137b5448327a70a3c895223478a0970
Aaditya Kumar <aadityakumar5892@gmail.com>
2026-06-04 16:01:51 +0530

    Time: 5 ms (29.61%), Space: 44.1 MB (23%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -4,13 +4,37 @@ class Solution {
         for(int s:stones){
             sum+=s;
         }
-        int [][] dp = new int[stones.length][sum+1];
-        for(int i = 0 ; i < dp.length ; i++){
-            for(int j = 0 ; j < dp[0].length ; j++){
-                dp[i][j] = -1;
+        // int [][] dp = new int[stones.length][sum+1];
+        // for(int i = 0 ; i < dp.length ; i++){
+        //     for(int j = 0 ; j < dp[0].length ; j++){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        int prev[] = new int[sum+1];
+        for(int i = 0 ; i <= sum ; i++){
+            if(stones[0] <= i){
+                prev[i] = stones[0];
+            }
+            else{
+                prev[i] = 0;
             }
         }
-        int max = f(stones, stones.length-1, sum/2, dp);
+
+        for(int i = 1 ; i < stones.length ; i++){
+            int[] curr = new int[sum+1];
+            for(int j = 0 ; j < sum+1 ; j++){
+                int n = prev[j];
+                int t = 0;
+                if(stones[i] <= j){
+                    t = stones[i]+prev[j-stones[i]];
+                }
+
+                curr[j] = Math.max(n, t);
+            }
+            prev = curr;
+        }
+        // int max = f(stones, stones.length-1, sum/2, dp);
+        int max = prev[sum/2];
         return (sum-2*max);
     }
 
