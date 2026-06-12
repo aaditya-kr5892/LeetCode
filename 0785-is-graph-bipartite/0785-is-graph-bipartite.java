20bbd86e092ceaa5c7a6cb8696239560ca7009b4
Aaditya Kumar <aadityakumar5892@gmail.com>
2026-06-12 21:04:30 +0530

    Time: 0 ms (100%), Space: 47 MB (88.48%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -2,34 +2,27 @@ class Solution {
     public boolean isBipartite(int[][] graph) {
         int color[] = new int[graph.length];
         Arrays.fill(color, -1);
-        
         for(int i = 0 ; i < graph.length ; i++){
             if(color[i] == -1){
-                if(f(graph, i, color) == false) return false;
+                boolean r = f(graph, color, 0, i);
+                if(r == false) return false;
             }
         }
         return true;
     }
-
-    boolean f(int[][] graph, int n, int[]color){
-        // int color[] = new int[graph.length];
-        Arrays.fill(color, -1);
-        Queue<Integer> que = new LinkedList<>();
-
-        que.add(n);
-        color[n] = 0;
-
-        while(!que.isEmpty()){
-            int node = que.poll();
-            for(int i = 0 ; i < graph[node].length ; i++){
-                if(color[graph[node][i]] == -1){
-                    color[graph[node][i]] = 1 - color[node];
-                    que.add(graph[node][i]);
-                }
-                else if(color[graph[node][i]] == color[node]){
-                    return false;
-                }
+    boolean f(int[][] graph, int[] color, int col, int node){
+        color[node] = col;
+        
+        for(int i = 0 ; i < graph[node].length ; i++){
+            if(color[graph[node][i]] == -1){
+                color[graph[node][i]] = 1-col;
+                boolean r = f(graph, color, 1-col, graph[node][i]);
+                if(r == false) return false;
             }
+            if(color[graph[node][i]] == color[node]){
+                return false;
+            }
+            
         }
         return true;
     }
