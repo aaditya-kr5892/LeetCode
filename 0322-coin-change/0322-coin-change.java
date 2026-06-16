2344a8514278550959e5a8db9edb15d6ca81a7d3
Aaditya Kumar <aadityakumar5892@gmail.com>
2026-06-16 15:15:03 +0530

    Time: 41 ms (7.08%), Space: 47.4 MB (11.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,36 +1,36 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        // int min[] = new int[1];
+        // min[0] = Integer.MAX_VALUE;
         int dp[][] = new int[coins.length][amount+1];
-        for(int i = 0 ; i < coins.length ; i++){
+        for(int i = 0 ; i < dp.length ; i++){
             for(int j = 0 ; j < dp[0].length ; j++){
                 dp[i][j] = -1;
             }
         }
-        int res = f(coins.length-1,coins,amount,dp) ;
-        if(res == (int)1e9)
-        return -1;
-        else
-        return res;
+        int min = f(coins, amount, 0, dp);
+        return min==Integer.MAX_VALUE?-1:min;
+
     }
-    int f(int ind,int []coins, int amount,int [][]dp){
-        if(amount == 0)
-        return 0;
-        if(ind == 0){
-            if(coins[ind] <= amount){
-                if(amount % coins[ind] == 0)
-                return amount/coins[ind];
-                else
-                return (int)1e9;
-            }
-            else
-            return (int)1e9;
+    int f(int[] coins, int amount, int i, int[][] dp){
+        if(amount == 0){
+            // min[0] = Math.min(min[0], num);
+            return 0;
         }
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-        int not = f(ind-1,coins,amount,dp);
-        int take = (int)1e9;
-        if(coins[ind] <= amount){
-            take = 1 + f(ind , coins,amount-coins[ind],dp);
+        if(i == coins.length){
+            // min[0] = Math.min(min[0], num);
+            return Integer.MAX_VALUE;
         }
-        return dp[ind][amount] = Math.min(take,not);
+
+        if(dp[i][amount] != -1) return dp[i][amount];
+
+        int take = Integer.MAX_VALUE;
+        if(coins[i] <= amount){
+            int t = f(coins, amount-coins[i], i, dp);
+            take = 1 + ((t==Integer.MAX_VALUE)?Integer.MAX_VALUE-1:t);
+        }
+        int notTake = f(coins, amount, i+1, dp);
+
+        return dp[i][amount] = Math.min(take, notTake);
     }
 }
