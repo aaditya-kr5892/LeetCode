794d628ba62b3643ee0a0843200038625c4a304a
Aaditya Kumar <aadityakumar5892@gmail.com>
2026-07-07 17:02:11 +0530

    Time: 16 ms (32.96%), Space: 70 MB (19.98%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,42 +9,18 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> l1 = new ArrayList<>();
-        List<TreeNode> l2 = new ArrayList<>();
-        f(root, p, l1);
-        f(root, q, l2);
-        int ind = -1;
-        for(int i = 0 ; i < Math.min(l1.size(), l2.size()) ; i++){
-            if(l1.get(i).val == l2.get(i).val){
-                ind = i;
-            }
-        }
-        return l1.get(ind);
-
+        return f(root, p, q);
     }
-    boolean f(TreeNode root, TreeNode p, List<TreeNode> list){
-        if(root == null){
-            return false;
+    TreeNode f(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null || root == p || root == q){
+            return root;
         }
-        if(root.val == p.val){
-            list.add(root);
-            return true;
-        }
-        boolean l = false, r = false;
+        
+        TreeNode l = f(root.left, p, q);
+        TreeNode r = f(root.right, p, q);
 
-        list.add(root);
-        if (f(root.left, p, list)) {
-            return true; 
-        }
-        
-        if (f(root.right, p, list)) {
-            return true;
-        }
-        
-        if(l == true || r == true){
-            return true;
-        }
-        list.remove(list.size()-1);
-        return false;
+        if(l != null && r != null) return root;
+
+        return (l != null)?l:r;
     }
 }
