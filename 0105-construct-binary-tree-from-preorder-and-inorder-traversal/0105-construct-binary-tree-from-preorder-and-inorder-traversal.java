7d2216615e2d590077f8b20436ff1932850c17d9
Aaditya Kumar <aadityakumar5892@gmail.com>
2026-07-10 23:07:57 +0530

    Time: 2 ms (64.94%), Space: 46.4 MB (50.44%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,23 +15,28 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return f(inorder, preorder, 0, inorder.length-1, 0, preorder.length-1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0 ; i < inorder.length ; i++){
+            map.put(inorder[i], i);
+        }
+        return f(inorder, preorder, 0, inorder.length-1, 0, preorder.length-1, map);
     }
-    TreeNode f(int[] inorder, int[] preorder, int in_s, int in_e, int pr_s, int pr_e){
+    TreeNode f(int[] inorder, int[] preorder, int in_s, int in_e, int pr_s, int pr_e, Map<Integer, Integer> map){
         if(in_s > in_e || pr_s > pr_e){
             return null;
         }
         int mid = -1;
         TreeNode r = new TreeNode(preorder[pr_s], null, null);
-        for(int i = in_s ; i <= in_e ; i++){
-            if(inorder[i] == preorder[pr_s]){
-                mid = i;
-                break;
-            }
-        }
+        // for(int i = in_s ; i <= in_e ; i++){
+        //     if(inorder[i] == preorder[pr_s]){
+        //         mid = i;
+        //         break;
+        //     }
+        // }
+        mid = map.get(preorder[pr_s]);
         int len = mid - in_s;
-        r.left = f(inorder, preorder, in_s, mid-1, pr_s+1, pr_s+len);
-        r.right = f(inorder, preorder, mid+1, in_e, pr_s+len+1, pr_e);
+        r.left = f(inorder, preorder, in_s, mid-1, pr_s+1, pr_s+len, map);
+        r.right = f(inorder, preorder, mid+1, in_e, pr_s+len+1, pr_e, map);
         return r;
     }
 }
