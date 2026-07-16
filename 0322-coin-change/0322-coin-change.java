fa25928a7bc170e07e3c6bc2ce1137c833cf24c3
Aaditya Kumar <aadityakumar5892@gmail.com>
2026-07-16 09:18:49 +0530

    Time: 48 ms (5.39%), Space: 47 MB (29.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,36 +1,28 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        // int min[] = new int[1];
-        // min[0] = Integer.MAX_VALUE;
-        int dp[][] = new int[coins.length][amount+1];
+        int[][] dp = new int[amount+1][coins.length+1];
         for(int i = 0 ; i < dp.length ; i++){
             for(int j = 0 ; j < dp[0].length ; j++){
                 dp[i][j] = -1;
             }
         }
-        int min = f(coins, amount, 0, dp);
-        return min==Integer.MAX_VALUE?-1:min;
-
+        int res = f(coins, amount, 0, dp);
+        return (res == (int)1e9)?-1:res;
     }
-    int f(int[] coins, int amount, int i, int[][] dp){
-        if(amount == 0){
-            // min[0] = Math.min(min[0], num);
-            return 0;
-        }
-        if(i == coins.length){
-            // min[0] = Math.min(min[0], num);
-            return Integer.MAX_VALUE;
+    int f(int[] coins, int amount, int ind, int[][] dp){
+        if(amount == 0) return 0;
+        if(ind == coins.length-1){
+            if(amount % coins[ind] == 0) return amount/coins[ind];
+            else return (int)1e9;
         }
 
-        if(dp[i][amount] != -1) return dp[i][amount];
+        if(dp[amount][ind] != -1) return dp[amount][ind];
 
-        int take = Integer.MAX_VALUE;
-        if(coins[i] <= amount){
-            int t = f(coins, amount-coins[i], i, dp);
-            take = 1 + ((t==Integer.MAX_VALUE)?Integer.MAX_VALUE-1:t);
+        int t = (int)1e9;
+        if(amount >= coins[ind]){
+            t = 1+f(coins, amount-coins[ind], ind, dp);
         }
-        int notTake = f(coins, amount, i+1, dp);
-
-        return dp[i][amount] = Math.min(take, notTake);
+        int nt = f(coins, amount, ind+1, dp);
+        return dp[amount][ind] = Math.min(t, nt);
     }
 }
