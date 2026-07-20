c8ca61a42034cfdd40fd148387937ed0fb13ecae
Aaditya Kumar <aadityakumar5892@gmail.com>
2026-07-20 14:26:14 +0530

    Time: 4 ms (76.77%), Space: 47.2 MB (35%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,31 +1,25 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()+1][word2.length()+1];
-        for(int i = 1 ; i < dp.length ; i++){
-            dp[i][0] = i;
-        }
-        for(int j = 1 ; j < dp[0].length ; j++){
-            dp[0][j] = j;
-        }
-        for(int i = 1 ; i < dp.length ; i++){
-            for(int j = 1 ; j < dp[0].length ; j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else
-                dp[i][j] = 1+Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
+        int [][] dp = new int[word1.length()][word2.length()];
+        for(int i = 0 ; i < dp.length ; i++){
+            for(int j = 0 ; j < dp[0].length ; j++){
+                dp[i][j] = -1;
             }
         }
-        return dp[word1.length()][word2.length()];
+        return f(word1, word2, 0, 0, dp);
     }
-    int f(int i, int j, String s1, String s2,int dp[][]){
-        if (i < 0) return j + 1;
-        if (j < 0) return i + 1;
-        if(dp[i][j] != -1) return dp[i][j] ;
-        if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = f(i-1,j-1,s1,s2,dp);
+    int f(String w1, String w2, int i, int j, int[][] dp){
+        if (i == w1.length()) return w2.length() - j;
+        if (j == w2.length()) return w1.length() - i;
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
-        return dp[i][j] = 1+Math.min(f(i-1,j,s1,s2,dp),Math.min(f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp)));
-        // return 
+        if(w1.charAt(i) == w2.charAt(j)){
+            return dp[i][j] = f(w1, w2, i+1, j+1, dp);
+        }
+        int in = f(w1, w2, i, j+1, dp);
+        int re = f(w1, w2, i+1, j+1, dp);
+        int de = f(w1, w2, i+1,j, dp);
+        return dp[i][j] = 1+Math.min(in, Math.min(re, de));
     }
 }
